40c1874ee6ec04d7d34162cccd18d2cc2cbff733
Karlie <heykarlie@gmail.com>
2021-08-23 14:52:05 -0700

    validation and some styling

--- src/components/NumberOfPeople.js
+++ src/components/NumberOfPeople.js
@@ -3,13 +3,21 @@ import { NumberContext } from './Handles';
 import {ReactComponent as Person} from '../images/icon-person.svg';
 
 const NumberOfPeople = () => {
-  const {ppl, handleSetPpl } = useContext(NumberContext);
+  const {bill, tip, customTip, ppl, handleSetPpl } = useContext(NumberContext);
+  let pplClass = '';
+  if (bill && (tip || customTip) && !ppl) {
+    pplClass = 'is-warn';
+  } else {
+    pplClass = '';
+  }
 
   return (
     <label className="form__label">
-      Number of People
-      <p>Can't be zero</p>
-      <div className="form__input">
+      <div className="form__ppl-labels">
+        <p className="form__ppl-label">Number of People</p>
+        <p className={`form__ppl-warn ${pplClass}`}>Can't be zero</p>
+      </div>
+      <div className={`form__input ${pplClass}`}>
           <Person />
           <input type="text" placeholder="0" maxLength="3" value={ppl} onChange={handleSetPpl} />
       </div>
